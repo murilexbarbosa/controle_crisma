6f8999786b3b8a2956ff9af5bec664f57ef4bdff
Murilo Barbosa <muka040789@hotmail.com>
2018-09-18 18:14:47 -0300

    criando models

--- catequista/models.py
+++ catequista/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Catequista(models.Model):
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    data_nascimento = models.DateField(verbose_name='Data Nascimento')
+    data_inicio = models.DateField(verbose_name='Data Início')
+    status = models.BooleanField(verbose_name='Status')
+
+    def __str__(self):
+            return self.nome
--- crismando/models.py
+++ crismando/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Crismando(models.Model):
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    data_nascimento = models.DateField(verbose_name='Data Nascimento')
+    nome_pai = models.CharField(max_length=100,blank=True, verbose_name='Nome do Pai')
+    nome_mae = models.CharField(max_length=100,blank=True, verbose_name='Nome da Mãe')
+    batismo = models.BooleanField(default=True, verbose_name='Batismo')
+    eucaristia = models.BooleanField(default=True, verbose_name='Eucaristica')
+    ano_crisma = models.CharField(max_length=10, verbose_name='Ano da Crisma')
+
+
+class CrismandoTelefone(models.Model):
+    TIPO_CHOICES = (
+        ('R', 'Residencial'),
+        ('C', 'Comercial'),
+        ('E', 'E-mail'),
+    )
+    crismando = models.ForeignKey(Crismando, on_delete=models.CASCADE, verbose_name='Crismando')
+    tipo = models.CharField(max_length=1, verbose_name='Tipo', choices=TIPO_CHOICES)
+    descricao = models.CharField(max_length=60)
+    

8358e788107e78ca9d660ef5fd8110838e8772b1
Murilo Barbosa <muka040789@hotmail.com>
2018-09-19 13:37:44 -0300

    add models

--- controle_pastoral/crisma/models.py
+++ controle_pastoral/crisma/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from crismando.models import Crismando
-from catequista.models import Catequista
+from controle_pastoral.crismando.models import Crismando
+from controle_pastoral.catequista.models import Catequista
 
 # Create your models here.
 class Turma(models.Model):
-    catequista = models.ForeignKey(Catequista, on_delete=models.CASCADE, verbose_name='Catequista')
-    crismando = models.ForeignKey(Crismando, on_delete=models.CASCADE, verbose_name='Crismando')
+    catequista = models.ManyToManyField(Catequista,  verbose_name='Catequista')
+    crismando = models.ManyToManyField(Crismando,  verbose_name='Crismando')
 
 
 class Encontro(models.Model):
-    crismando = models.ForeignKey(Crismando, on_delete=models.CASCADE, verbose_name='Crismando')
+    crismando = models.ManyToManyField(Crismando,verbose_name='Crismando')
     presenca = models.BooleanField(verbose_name='Presença', default=False)
     data_encontro = models.DateField(verbose_name='Data Encontro')
     observacao = models.TextField(verbose_name='Observação', blank=True, null=True)
--- controle_pastoral/settings.py
+++ controle_pastoral/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    ''
+    'controle_pastoral.crisma',
+    'controle_pastoral.crismando',
+    'controle_pastoral.catequista',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 

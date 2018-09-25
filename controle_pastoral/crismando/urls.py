cec8d1d39a86a38c878ec785485df602dad782b3
Murilo Barbosa <muka040789@hotmail.com>
2018-09-25 17:20:12 -0300

    tela de cadastro de crismando

--- controle_pastoral/crismando/urls.py
+++ controle_pastoral/crismando/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'main-crismando/', views.main_crismando, name='main_crismando'),
+    url(r'novo-crismando/', views.novo_crismando, name='novo_crismando'),
 ]
--- controle_pastoral/crismando/views.py
+++ controle_pastoral/crismando/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Crismando
+from .forms import CrismandoForm
 
 # Create your views here.
 def main_crismando(request):
     return render(request, 'crismando/main_crismando.html')
+
+def novo_crismando(request):
+    if request.method == 'POST':
+        form = CrismandoForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('main_crismando')
+        else:
+            print(form.errors)
+    else:
+        form = CrismandoForm()
+    return render(request, 'crismando/novo_crismando.html',{'form': form})
